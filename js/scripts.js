78fc582dfb4f7285eb8cd7b3e721aa0427aea492
sobudo <sobudo@gmail.com>
2018-02-27 11:17:11 +0300

    Added new properties to js and increased prototype list

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.street = street;
+  this.city = city;
+  this.country = country;
 }
 
 Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+  return this.firstName + " " + this.lastName + " " + this.street + " " + this.city + " " + this.country;
 }
 
 // user interface logic
@@ -15,8 +18,11 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var inputtedFirstName = $("input#new-street").val();
+    var inputtedLastName = $("input#new-city").val();
+    var inputtedLastName = $("input#new-country").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedStreet, inputtedCity, inputtedCountry);
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
@@ -25,9 +31,15 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $(".first-name").text(newContact.street);
+      $(".last-name").text(newContact.city);
+      $(".last-name").text(newContact.country);
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-country").val("");
   });
 });
